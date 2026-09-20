eb5aa4235de945fc5c772345dc5638c81b0a36a8
walaphysics73-coder <walaphysics73@gmail.com>
2026-09-20 14:01:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def analyze_prescription(image_bytes: bytes) -> str:
     """
 
     response = ai_client.models.generate_content(
-       model='gemini-2.5-flash-latest',
+      model='gemini-1.5-flash',
         contents=[
             types.Part.from_bytes(data=image_bytes, mime_type='image/jpeg'),
             prompt
